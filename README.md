4dc01590173dc5d9d33d192fb1e77ab64a8fc0e1
Cheng Jed <jed351@Chengs-MBP.broadband>
2023-04-10 01:25:03 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# telegram_json_parser
+# Telegram Json Parser
+
+This is a (very) simple CLI tool to parser and search for messages in a json file exported from Telegram with the help from [simdjson](https://github.com/simdjson/simdjson).
+
+It should work for mac and linux. I don't have a windows machine.
+
+## Usage
+
+1. Download the official Telegram desktop client [official Telegram client](https://desktop.telegram.org/) . As far as I know, this is not supported by any third party clients.
+
+2. Export your chat history. You can do this by going to the chat you want to export, click on the three dots on the top right corner, and select "Export Chat History". Export the chat as a JSON file. Remember to select the option to exclude media files if you don't want to export them.
+
+3. Compile the program with `c++ -o main main.cpp simdjson.cpp -std=c++11 `
+
+4. Run the program './main <path to json file>'
+
+5. Search for messages by typing the keyword you want to search for. 
+
+6. Type 'exit' to exit the program.
---
+++ main.cpp
@@ -0,0 +1,104 @@
+#include "simdjson.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <future>
+#include <algorithm>
+
+
+using namespace simdjson;
+using namespace std;
+
+
+
+
+int main(int argc, char *argv[]) {
+    // Parse the arguments
+    string_view file_name(argv[1]); /*file name*/
+    string_view str_message;
+    
+    // count how many message is found
+    int count;
+    
+    
+    // initlize the json parser
+    ondemand::parser parser;
+    padded_string json = padded_string::load(file_name);
+    ondemand::document doc = parser.iterate(json);
+
+    // get the messages
+    ondemand::array messages = doc.find_field("messages").get_array();
+
+
+    // initlize the vector to store the messages
+    vector<string_view> messages_vector;
+
+    for (auto value: messages){
+        auto text = value.find_field("text");
+
+        ondemand::json_type type = text.type();
+        
+        // If the message only contains text
+        if (type == ondemand::json_type::string){
+            auto error = text.get_string().get(str_message);
+            messages_vector.push_back(string_view(str_message));
+        }
+        
+        // the message might contain hyper links
+        // the structure of the message would be different
+        else if (type == ondemand::json_type::array){
+            for (auto sub_value: text){
+                auto error = sub_value.get_string().get(str_message);
+
+                if (!error){
+                    messages_vector.push_back(string_view(str_message));
+                } 
+            }
+   
+        }
+    }
+    
+
+
+
+    string str_keyword;
+    string_view str_keyword_view;
+
+    std::vector<std::string_view> result;
+
+    while (true){
+        cout << "Type the keyword you are looking for (type 'exit' to quit): ";
+        getline(cin, str_keyword);
+
+        str_keyword_view = string_view(str_keyword);
+
+        if (str_keyword == "exit"){
+            break;
+        }
+
+
+        
+
+        for (auto& str: messages_vector){
+            if (str.find(str_keyword_view) != string_view::npos){
+                result.push_back(str);
+            }
+        }
+
+        if (result.size() == 0){
+            cout << "No result found" << endl;
+        }
+        else{
+            for (auto& str: result){
+                cout << str << endl;
+            }
+        }
+        
+        result.clear();
+
+    }
+
+}
+
+    
+
